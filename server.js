e3927ad85856f98128e3d12e1604e58f75c06b55
keevsz <keviv1q2@gmail.com>
2023-11-15 20:09:51 -0500

    Change index response message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hi!')
+  res.send('<h1>AWS sv</h1>')
 })
 app.listen(3000)
 console.log('Server running on port 3000')
