c25f0479085585e6bb9a75e56015caa94970874a
keevsz <keviv1q2@gmail.com>
2023-11-15 20:20:10 -0500

    Change port

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('<h1>AWS sv</h1>')
 })
-app.listen(3000)
-console.log('Server running on port 3000')
+app.listen(8080)
+console.log('Server running on port 8080')
